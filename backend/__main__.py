7d4b09c6a6d0205c50f154b8d4405abf84f5c37f
yankovskaya <yankovskaya.my@gmail.com>
2022-03-10 10:06:49 +0300

    Update main.py

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, Blueprint
+from flask import Flask
 from backend.users import user
 
 logger = logging.getLogger(__name__)
@@ -8,10 +8,9 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(level=logging.DEBUG)
     logger.info("application started")
+    app = Flask(__name__)
+    app.register_blueprint(user, url_prefix='/api/v1/users')
     app.run(host='0.0.0.0', port=8080, debug=False)
-
-app = Flask(__name__)
-app.register_blueprint(user, url_prefix='/api/u/users')
 
 if __name__ == '__main__':
     main()
