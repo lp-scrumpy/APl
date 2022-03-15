7e8613c96fdb601643eac6866ab45816326d5bcf
yankovskaya <yankovskaya.my@gmail.com>
2022-03-15 19:35:58 +0300

    Fix bugs in db.py

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('postgresql://nrdhmlja:Wns0JwGi7_J-CaDAAKC9-LmUVKTtJo38@balarama.db.elephantsql.com:5432/nrdhmlja')
+from backend.settings import DB_URL
+
+engine = create_engine(DB_URL)
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
---
+++ backend/settings.py
@@ -0,0 +1,1 @@
+DB_URL = 'postgresql://nrdhmlja:Wns0JwGi7_J-CaDAAKC9-LmUVKTtJo38@balarama.db.elephantsql.com:5432/nrdhmlja'
