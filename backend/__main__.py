ea3a27328299fe4b0bd745a72279ced030830fa4
yankovskaya <yankovskaya.my@gmail.com>
2022-03-19 10:49:47 +0300

    Add exception handling/

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,9 +1,25 @@
 import logging
+from http import HTTPStatus
+
 from flask import Flask
+from pydantic import ValidationError
+from werkzeug.exceptions import HTTPException
+
+from backend.errors import AppError
 from backend.views.users import user
 
 logger = logging.getLogger(__name__)
 
+def handle_http_exceptions(error: HTTPException):
+    return {'message': error.description}, error.code
+
+
+def handle_app_error(error: AppError):
+    return {'message': error.reason}, error.status
+
+
+def handle_validation_error(error: ValidationError):
+    return error.json(), HTTPStatus.BAD_REQUEST
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
