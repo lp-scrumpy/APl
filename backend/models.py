6b98287eab16596f5e1a786b9f338f144a2e7933
Elijah Radchenko <elijah.radchenko@gmail.com>
2022-03-30 17:12:28 +0800

    Реализована модель БД

--- backend/models.py
+++ backend/models.py
@@ -1,47 +1,43 @@
-from sqlalchemy import Column, Integer, String, DateTime, Date, Table, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from backend.db import Base, engine
 from sqlalchemy.orm import relationship
-
-class User(Base):
-    __tablename__ = 'users'
-    uid = Column(Integer, primary_key=True)
-    estimate_uid = Column(Integer, ForeignKey('estimates.uid'))
-    username = Column(String)
-    children = relationship("Planning")
-    def __repr__(self):
-        return f'<User {self.uid} {self.username} {self.estimate_id}>'
-
-
-class Planning(Base):
-    __tablename__ = 'plannings'
-    uid = Column(Integer, primary_key=True)
-#    task_uid = Column(Integer, ForeignKey('task.id'))
-#    user_uid = Column(Integer, ForeignKey('user.id'))
-    name = Column(String)
-    date = Column(DateTime)
-def __repr__(self):
-        return f'<Planning {self.uid} {self.name} {self.date}>'
 
 
 class Estimate(Base):
     __tablename__ = 'estimates'
     uid = Column(Integer, primary_key=True)
-    meaning = Column(String, primary_key=True)
-    children = relationship("Task")
-    children = relationship("User")
+    storypoint = Column(String)
+    user_id = Column(Integer, ForeignKey('users.uid'))
+    task_id = Column(Integer, ForeignKey('tasks.uid'))
 def __repr__(self):
-        return f'<Estimate {self.uid} {self.meaning}>'
+        return f'<Estimate {self.uid} {self.meaning} {self.user_id} {self.task_id}>'
 
+class User(Base):
+    __tablename__ = 'users'
+    uid = Column(Integer, primary_key=True)
+    planning_id = Column(Integer, ForeignKey('plannings.uid'))
+    name = Column(String)
+    children = relationship("Estimate")
+    def __repr__(self):
+        return f'<User {self.uid} {self.planning_id} {self.user_name}>'
 
 class Task(Base):
     __tablename__ = 'tasks'
     uid = Column(Integer, primary_key=True)
-#    estimate_uid = Column(Integer, ForeignKey('estimate.id'))
-    taskname = Column(String)
-    children = relationship("Planning")
+    planning_id = Column(Integer, ForeignKey('plannings.uid'))
+    name = Column(String)
 def __repr__(self):
-        return f'<Task {self.uid} {self.estimate_id} {self.taskname}>'
+        return f'<Task {self.uid} {self.planning_id} {self.task_name}>'
 
+class Planning(Base):
+    __tablename__ = 'plannings'
+    uid = Column(Integer, primary_key=True)
+    name = Column(String)
+    date = Column(DateTime)
+    children = relationship('Task')
+    children = relationship('User')
+def __repr__(self):
+        return f'<Planning {self.uid} {self.name} {self.date}>'
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
